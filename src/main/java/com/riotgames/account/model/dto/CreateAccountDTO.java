48607e2c560054e98dd4b72702c5232acf253166
LucasAlme <lucasleesin2001@gmail.com>
2020-09-26 19:12:48 -0300

    Remocao dos getters e setters

--- src/main/java/com/riotgames/account/model/dto/CreateAccountDTO.java
+++ src/main/java/com/riotgames/account/model/dto/CreateAccountDTO.java
@@ -21,41 +21,5 @@ public class CreateAccountDTO implements Serializable {
 	private String password;
 	private Boolean isTerms;
 	private Boolean isAdEmail;
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getDateBirth() {
-		return dateBirth;
-	}
-	public void setDateBirth(String dateBirth) {
-		this.dateBirth = dateBirth;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Boolean getIsTerms() {
-		return isTerms;
-	}
-	public void setIsTerms(Boolean isTerms) {
-		this.isTerms = isTerms;
-	}
-	public Boolean getIsAdEmail() {
-		return isAdEmail;
-	}
-	public void setIsAdEmail(Boolean isAdEmail) {
-		this.isAdEmail = isAdEmail;
-	}
 
 }
